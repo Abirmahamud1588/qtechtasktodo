7e4364c4071460a5567486d8f1fd7d58efafb066
Abirmahamud1588 <abirmahamud1998@gmail.com>
2024-02-08 01:21:51 +0600

    new task done

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const GivenTasks = [
   { id: 1, title: "cricket", status: "incomplete", priority: "low" },
   { id: 2, title: "cricket 2", status: "incomplete", priority: "medium" },
@@ -10,6 +12,26 @@ const priorities = {
 };
 
 const App = () => {
+  const [tasks, setTasks] = useState(GivenTasks);
+  const [totalTasks, setTotalTasks] = useState(GivenTasks.length);
+  const [newtaskTitle, setNewTaskTitle] = useState("");
+  const [newTaskPriority, setNewTaskPriority] = useState("low");
+  const [completedTasks, setCompletedTasks] = useState(
+    GivenTasks.filter((task) => task.status === "completed").length
+  );
+  const handleAddedTask = () => {
+    if (newtaskTitle.trim() !== "") {
+      const newTask = {
+        id: tasks.length + 1,
+        title: newtaskTitle,
+        status: "incomplete",
+        priority: newTaskPriority,
+      };
+      setTasks([...tasks, newTask]);
+      setNewTaskTitle("");
+      setTotalTasks(totalTasks + 1);
+    }
+  };
   return (
     <div>
       <div className="App min-h-screen bg-gray-100 flex flex-col justify-center items-center">
@@ -21,21 +43,30 @@ const App = () => {
             <input
               type="text"
               placeholder="Enter task"
+              value={newtaskTitle}
+              onChange={(e) => setNewTaskTitle(e.target.value)}
               className="mr-2 py-1 px-2 border border-gray-300 rounded-md flex-grow"
             />
-            <select className="py-1 px-2 border border-gray-300 rounded-md">
+            <select
+              value={newTaskPriority}
+              onChange={(e) => setNewTaskPriority(e.target.value)}
+              className="py-1 px-2 border border-gray-300 rounded-md"
+            >
               {Object.keys(priorities).map((priority) => (
                 <option key={priority} value={priority}>
                   {priorities[priority].label}
                 </option>
               ))}
             </select>
-            <button className="bg-blue-500 text-white py-1 px-4 rounded-md ml-2 hover:bg-blue-600">
+            <button
+              onClick={handleAddedTask}
+              className="bg-blue-500 text-white py-1 px-4 rounded-md ml-2 hover:bg-blue-600"
+            >
               Add Task
             </button>
           </div>
-          <p className="mb-2">Total Tasks: </p>
-          <p>Completed Tasks: </p>
+          <p className="mb-2">Total Tasks: {totalTasks}</p>
+          <p>Completed Tasks: {completedTasks}</p>
         </div>
       </div>
     </div>
